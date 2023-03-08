773aadd370247663ca89f35a123220206f7c7bdd
micah-hirsch <micahhirsch06@gmail.com>
2023-03-08 14:24:04 -0500

    Beginning preliminary analysis preprocessing script

---
+++ Preliminary Analysis/Data Preprocessing.R
@@ -0,0 +1,30 @@
+# Listening Effort Preliminary Analysis: Data Preprocessing 
+
+# Author: Micah E. Hirsch, M.S., mhirsch@fsu.edu
+
+# Purpose: To load in edf files from EyeLink and process the raw data 
+# files to prepare for analysis.
+
+# Load Packages
+
+library(gazer) #remotes::install_github("dmirman/gazer")
+library(tidyverse) #install.packages("tidyverse")
+library(zoo) # install.packages("zoo")
+library(knitr) 
+library(devtools)
+library(edfR) #install_github("jashubbard/edfR")
+
+# Set working directories
+
+edf.path <- "iCloud Drive/Documents/Listening Effort Experiment/results"
+csv.path <- "iCloud Drive/Documents/Listening-Effort-in-Dysarthria/Preliminary Analysis/Prepped_Data"
+
+# Loading and Reading the edf files from Pilot Collection
+
+##Loads and parses each edf file
+file_list_edf <- list.files(path = edf.path, pattern = ".edf")
+parse_edf(file_list = file_list_edf, output_dir = csv.path, type = "pupil")
+
+## saving csv file list and merging all sample files 
+file_list_csv <- list.files(path = csv.path, pattern = ".csv")
+sample_gaze_data <- merge_gazer_files(file_list_csv, type = "edf")
