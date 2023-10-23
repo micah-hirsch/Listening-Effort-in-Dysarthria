0cfef7aefedf5de88edcf82828c328aac5a7d26c
Micah Hirsch <micahhirsch06@gmail.com>
2023-10-23 12:31:31 -0400

    Reorganized analysis files and updated gitignore

---
+++ ASHA 2023 Analysis/pupil_data_preprocessing.R
@@ -0,0 +1,11 @@
+# Pupil Data Preprocessing
+
+# Author: Micah E. Hirsch
+
+# Date: 10/23/2023
+
+## Purpose: To load in raw data from  Eyelink and prepare pupil data for analysis and visualization.
+## This data processing and analysis is being completed for a poster presentation 
+## at the Annual American Speech Language Hearing Association Convention being
+## held in Boston, MA in November 2023.
+
