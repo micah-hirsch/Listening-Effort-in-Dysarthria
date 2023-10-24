76e30ab1ec421283ef86bed1e60516628e4c6e53
Micah Hirsch <micahhirsch06@gmail.com>
2023-10-24 11:10:31 -0400

    Finished for loop for loading pupil data

--- ASHA 2023 Analysis/pupil_data_preprocessing.R
+++ ASHA 2023 Analysis/pupil_data_preprocessing.R
@@ -5,9 +5,9 @@
 # Date: 10/23/2023
 
 ## Purpose: To load in raw data from Eyelink and prepare pupil data viz and analysis.
-## This data processing and analysis is being completed for a poster presentation 
-## at the Annual American Speech Language Hearing Association Convention being
-## held in Boston, MA in November 2023.
+## This particular data processing and analysis is being completed for a poster 
+## presentation at the Annual American Speech Language Hearing Association Convention 
+## being held in Boston, MA in November 2023.
 
 # Loading Required Packages
 
@@ -35,28 +35,59 @@ data_list <- list()
 ## Initiating loop to load in data for each participant
 for (file in file_list) {
   
-  # Import file
+  ## Import file
   data <- rio::import(file) 
   
-  data <- data %>%
-    # Creating pupil and blink variables. 
-    # Most participants had their right eye tracked, but some had their left eye tracked.
-    dplyr::mutate(pupil = ifelse(EYE_TRACKED == "Right", RIGHT_PUPIL_SIZE, LEFT_PUPIL_SIZE),
-                  blink = ifelse(EYE_TRACKED == "Right", RIGHT_IN_BLINK, LEFT_IN_BLINK)) %>%
-    # Bring variable names to lower case
-    dplyr::rename_all(., .funs = tolower) %>%
-    # Renaming variables so it is consistent with gazer's requirements
-    dplyr::rename(subject = recording_session_label,
-                  trial = trial_index) %>%
-    # Selecting needed variables for processing
-    dplyr::select(subject, trial, eye_tracked, blink, timestamp, pupil, ip_start_time, 
-                  sample_message, effort_rating, code, speaker, practicetrial, 
-                  targetphrase, counterbalance)
+  ## For subject id's ending in _2, we have to discard their first 15 trials
+  ## because the experiment was restarted for them after the 15th trial
+  ## (due to technical errors). The first 15 trials were recorded in 
+  ## the initial output file for the participant.
+  if (any(grepl("_2", data$RECORDING_SESSION_LABEL))) {
+    
+    data <- data %>%
+      # Creating pupil and blink variables. 
+      # Most participants had their right eye tracked, but some had their left eye tracked.
+      dplyr::mutate(pupil = ifelse(EYE_TRACKED == "Right", RIGHT_PUPIL_SIZE, LEFT_PUPIL_SIZE),
+                    blink = ifelse(EYE_TRACKED == "Right", RIGHT_IN_BLINK, LEFT_IN_BLINK)) %>%
+      # Bring variable names to lower case
+      dplyr::rename_all(., .funs = tolower) %>%
+      # Renaming variables so it is consistent with gazer's requirements
+      dplyr::rename(subject = recording_session_label,
+                    trial = trial_index) %>%
+      # Selecting needed variables for processing
+      dplyr::select(subject, trial, eye_tracked, blink, timestamp, pupil, ip_start_time, 
+                    sample_message, effort_rating, code, speaker, practicetrial, 
+                    targetphrase, counterbalance) %>%
+      # Removing the first 15 trials
+      dplyr::filter(!trial %in% (1:15)) %>%
+      # removing _2 from these subject ids
+      dplyr::mutate(subject = str_replace(subject, "_2", ""))
+    
+  } else {
+    
+    ## This block of code does the same thing as above, except the first 15 trials
+    ## are not removed
+    data <- data %>%
+      dplyr::mutate(pupil = ifelse(EYE_TRACKED == "Right", RIGHT_PUPIL_SIZE, LEFT_PUPIL_SIZE),
+                    blink = ifelse(EYE_TRACKED == "Right", RIGHT_IN_BLINK, LEFT_IN_BLINK)) %>%
+      dplyr::rename_all(., .funs = tolower) %>%
+      dplyr::rename(subject = recording_session_label,
+                    trial = trial_index) %>%
+      dplyr::select(subject, trial, eye_tracked, blink, timestamp, pupil, ip_start_time, 
+                    sample_message, effort_rating, code, speaker, practicetrial, 
+                    targetphrase, counterbalance)
+    
+  }
+  
 
-  # Add each participant's data to the empty data list
+  ## Add each participant's data to the empty data list
   data_list[[length(data_list)+1]] <- data
   
 }
 
-# Merging each participant's data into one dataframe
+## Merging each participant's data into one dataframe
 pupil_data <- do.call(rbind, data_list)
+
+## Removing unneeded items from the environment
+
+rm(data, data_list, file, file_list)
