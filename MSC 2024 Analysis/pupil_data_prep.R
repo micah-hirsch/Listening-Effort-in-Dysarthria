aadfd26eb318ca9d782ed1693e5ad3a4db3e0c85
Micah Hirsch <micahhirsch06@gmail.com>
2024-02-06 14:13:49 -0500

    cleaned and exported pupil data

--- MSC 2024 Analysis/pupil_data_prep.R
+++ MSC 2024 Analysis/pupil_data_prep.R
@@ -122,14 +122,100 @@ pupil_data2 <- pupil_data |>
 
 
 ## Filter out unneeded rows and trials
-trimmed_pupil_data <- pupil_data2 %>%
+trimmed_pupil_data <- pupil_data2 |>
   ## Filtering out rows before trial start and after trial end
-  dplyr::filter(timestamp >= start_time & timestamp <= end_time) %>%
+  dplyr::filter(timestamp >= start_time & timestamp <= end_time) |>
   ## Removing practice trials from df
-  dplyr::filter(practicetrial != 'Practice') %>%
-  dplyr::select(!practicetrial) %>%
+  dplyr::filter(practicetrial != 'Practice') |>
+  dplyr::select(!practicetrial) |>
   ## Aligning data to onset of phrase presentation
-  dplyr::mutate(time_c = timestamp - phrase_start) %>%
+  dplyr::mutate(time_c = timestamp - phrase_start) |>
   ## Removing unneeded variables
-  dplyr::select(!c(timestamp, start_time:end_time)) %>%
+  dplyr::select(!c(timestamp, start_time:end_time)) |>
   dplyr::relocate(time_c, .after = pupil)
+
+## Removing trials/participants with too much data loss
+trimmed_pupil_data <- gazer::count_missing_pupil(trimmed_pupil_data, pupil = "pupil", missingthresh = 0.2)
+
+## Remove unneeded items from environment 
+rm(phrase_end, phrase_start, pupil_data, pupil_data2, trial_end, trial_start)
+
+# Fill in missing data from blinks
+
+## Deblinking
+pupil_extend <- trimmed_pupil_data |>
+  dplyr::group_by(subject, trial) |>
+  dplyr::mutate(extendpupil = extend_blinks(pupil, 
+                                            fillback = 50, 
+                                            fillforward = 160, 
+                                            hz = 1000))
+
+## linear interpolation
+interp <- interpolate_pupil(pupil_extend,
+                            extendblinks = T, 
+                            type = "linear", 
+                            hz = 1000)
+
+## 10 Hz 5-point moving average filter
+smoothed <- interp |>
+  dplyr::mutate(smoothed_pupil = moving_average_pupil(interp, n = 5)) |>
+  ## Selecting relevant variables
+  dplyr::select(c(subject, trial, sample_message, time_c, effort_rating, 
+                  code, speaker, targetphrase, counterbalance,smoothed_pupil)) |>
+  dplyr::relocate(smoothed_pupil, .after = time_c) |>
+  dplyr::rename(time = time_c)
+
+# Baseline correction
+
+baseline_pupil <- baseline_correction_pupil(smoothed, pupil_colname = "smoothed_pupil",
+                                            baseline_window = c(-500, 0))
+
+# Artifact Rejection
+
+## Creating histogram to detect potential spurious tracking
+baseline_pupil |>
+  ggplot() +
+  aes(x = pup_interp) + 
+  geom_histogram(color = "green", binwidth = 0.5) +
+  geom_vline(xintercept = 2750, linetype = "dotted") +
+  theme_bw()
+
+## Looking for rapid changes in pupil dilation using median absolute deviation
+mad_removal <- baseline_pupil |>
+  dplyr::group_by(subject, trial) |>
+  dplyr::mutate(speed = speed_pupil(baselinecorrectedp, time)) |>
+  dplyr::mutate(MAD = calc_mad(speed, n=16)) |>
+  dplyr::filter(speed < MAD)
+
+## Proportion of rows removed (as of 2/6/2024: 1.30%)
+((nrow(baseline_pupil) - nrow(mad_removal)) / nrow(baseline_pupil)) * 100
+
+### Checking to see if whole trials were removed from any of the participants (1 trial from LE10)
+trial_check <- mad_removal |>
+  select(subject, trial) |>
+  distinct() |>
+  group_by(subject) |>
+  summarize(n = n())
+
+## Removing unneeded items from the environment
+rm(baseline_pupil, interp, pupil_extend, smoothed, trimmed_pupil_data, trial_check)
+
+# Downsampling
+
+bin.length <- 20
+
+data.binned <- mad_removal %>%
+  mutate(timebins = round(time/bin.length)*bin.length) %>%
+  dplyr::group_by(subject, trial, speaker, timebins, effort_rating,
+                  code, targetphrase, counterbalance) %>%
+  dplyr::summarize(pupil.binned = mean(baselinecorrectedp)) %>%
+  dplyr::ungroup()
+
+# Export Cleaned Data
+
+## Set working directory
+setwd("~/Documents/Listening-Effort-in-Dysarthria/MSC 2024 Analysis/Cleaned Data")
+
+## Export
+rio::export(data.binned, "cleaned_pupil_data.csv")
+
